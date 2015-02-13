947640dc599e98ef84ef9fd5227017911c999a86
Pyry Jahkola <pyry.jahkola@iki.fi>
2015-02-13 17:10:05 +0200

    Share build schemes. Make tests run on Mac

--- Allsorts/Orderable.swift
+++ Allsorts/Orderable.swift
@@ -11,28 +11,37 @@ infix operator <=> {
     precedence 131 // one higher than ==, !=, <, >, <= and >=
 }
 
+/// Implementation detail for the `Orderable` protocol.
 public protocol _Orderable {
     func <=> (lhs: Self, rhs: Self) -> Ordering
 }
 
-public protocol Orderable : Comparable, _Orderable {}
+/// A type that can be efficiently three-way compared with another value of its
+/// type using the `<=>` operator.
+///
+/// Any `Comparable` can be made `Orderable` by simply declaring protocol
+/// conformance in an extension, but some types (like `Swift.String`) may have a
+/// more efficient implementation for `<=>`.
+public protocol Orderable : _Orderable, Comparable {}
 
-public func <=> <T : Comparable>(lhs: T, rhs: T) -> Ordering {
-    if lhs < rhs { return .LT }
-    if rhs < lhs { return .GT }
-    assert(lhs == rhs)
-    return .EQ
+/// Default implementation for making `Comparable` types `Orderable`.
+public func <=> <T : Comparable>(left: T, right: T) -> Ordering {
+    return Ordering.compare(left, right)
 }
 
-public func == <T : Orderable>(lhs: T, rhs: T) -> Bool {
-    switch lhs <=> rhs {
+/// Default implementation for making `Orderable` types `Equatable`. Override
+/// if needed.
+public func == <T : Orderable>(left: T, right: T) -> Bool {
+    switch left <=> right {
     case .EQ: return true
     default:  return false
     }
 }
 
-public func < <T : Orderable>(lhs: T, rhs: T) -> Bool {
-    switch lhs <=> rhs {
+/// Default implementation for making `Orderable` types `Comparable`. Override
+/// if needed.
+public func < <T : Orderable>(left: T, right: T) -> Bool {
+    switch left <=> right {
     case .LT: return true
     default:  return false
     }
