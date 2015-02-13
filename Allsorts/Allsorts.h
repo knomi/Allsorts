b868e850053cd1e820d0334b502aa86e8aa5f52e
Pyry Jahkola <pyry.jahkola@iki.fi>
2015-02-13 12:52:25 +0200

    Initial commit

---
+++ Allsorts/Allsorts.h
@@ -0,0 +1,19 @@
+//
+//  Allsorts.h
+//  Allsorts
+//
+//  Created by Pyry Jahkola on 13.02.2015.
+//  Copyright (c) 2015 Pyry Jahkola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Allsorts.
+FOUNDATION_EXPORT double AllsortsVersionNumber;
+
+//! Project version string for Allsorts.
+FOUNDATION_EXPORT const unsigned char AllsortsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Allsorts/PublicHeader.h>
+
+
