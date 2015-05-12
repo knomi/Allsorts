f820745bcb42a20935c7a6aefcbf516af4b9fb64
Pyry Jahkola <pyry.jahkola@iki.fi>
2015-05-12 11:17:13 +0300

    Move comparator combinators to own file

---
+++ Allsorts/Combinators.swift
@@ -0,0 +1,37 @@
+//
+//  Combinators.swift
+//  Allsorts
+//
+//  Copyright (c) 2015 Pyry Jahkola. All rights reserved.
+//
+
+/// Evaluate the lexicographic ordering of two comparison expressions. If `left`
+/// evaluates not-equal, return its result. Else, evaluate and return `right`.
+public func || (left: Ordering, @autoclosure right: () -> Ordering) -> Ordering {
+    switch left {
+    case .LT: return .LT
+    case .EQ: return right()
+    case .GT: return .GT
+    }
+}
+
+/// Lexicographical comparator composing operator. Symbol chosen because of its
+/// similarity to the OR operator: returns the inequality in the left operand,
+/// or else the result of the right-hand side.
+infix operator <|> {
+    associativity right
+    precedence 121 // one higher than `&&`, and lower than `==` etc.
+}
+
+/// Compose two comparators lexicographically: the function short-circuits if
+/// the `left` comparator evaluates different than `Ordering.EQ`. Otherwise,
+/// evaluates and returns the result of the `right` comparator.
+///
+/// **Remark:** This operator is useful together with the use of `Ordering.by`.
+///
+/// **See also:** `Ordering.by`. `sorted`, `stableSorted`
+public func <|> <Args>(left: Args -> Ordering, right: Args -> Ordering) -> Args -> Ordering {
+    return {args in
+        left(args) || right(args)
+    }
+}
--- Allsorts/Ordering.swift
+++ Allsorts/Ordering.swift
@@ -57,34 +57,3 @@ extension Ordering : Printable {
         }
     }
 }
-
-/// Evaluate the lexicographic ordering of two comparison expressions. If `left`
-/// evaluates not-equal, return its result. Else, evaluate and return `right`.
-public func || (left: Ordering, @autoclosure right: () -> Ordering) -> Ordering {
-    switch left {
-    case .LT: return .LT
-    case .EQ: return right()
-    case .GT: return .GT
-    }
-}
-
-/// Lexicographical comparator composing operator. Symbol chosen because of its
-/// similarity to the OR operator: returns the inequality in the left operand,
-/// or else the result of the right-hand side.
-infix operator <|> {
-    associativity right
-    precedence 121 // one higher than `&&`, and lower than `==` etc.
-}
-
-/// Compose two comparators lexicographically: the function short-circuits if
-/// the `left` comparator evaluates different than `Ordering.EQ`. Otherwise,
-/// evaluates and returns the result of the `right` comparator.
-///
-/// **Remark:** This operator is useful together with the use of `Ordering.by`.
-///
-/// **See also:** `Ordering.by`. `sorted`, `stableSorted`
-public func <|> <Args>(left: Args -> Ordering, right: Args -> Ordering) -> Args -> Ordering {
-    return {args in
-        left(args) || right(args)
-    }
-}
